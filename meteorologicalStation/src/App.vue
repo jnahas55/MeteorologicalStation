862703a25b9acf57a4976d5aca24ea63e0b45dfa
jnahas55 <jorge.nahas55@gmail.com>
2018-09-05 01:08:58 -0300

    Adding initial structure and dashboard view

---
+++ meteorologicalStation/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <station-header></station-header>
+    <dashboard id="content"></dashboard>
+    
+  </div> 
+  
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Header from './components/Header.vue'
+import Dashboard from './components/Dashboard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    'dashboard': Dashboard,
+    'station-header': Header
+  }
+}
+</script>
+
+<style>
+
+#content{
+    margin-top: 9.5%
+
+}
+
+</style>
---
+++ meteorologicalStation/src/components/Dashboard.vue
@@ -0,0 +1,149 @@
+<template>
+    <div class="container">
+    
+        <div class="row d-flex align-items-stretch" style="font-family: sans-serif;">
+
+          <div class="col-md-4" style="font-family: sans-serif; margin-bottom:3.5%;">
+          
+            <div class="card shadow clickable-card" >
+              <div class="card-body text-center">
+                <div class="row">
+                  <div class="col-md-6">               
+                    <!--i class="fas fa-tint" style="font-size:40px;"></i-->
+                    <img src="../../../../humidity.png" alt="Responsive image" class="img-fluid">
+                  </div>
+                  <div class="col-md-6">
+                    <h2 class="card-title text-primary">63%</h2>
+                    <div class="text-muted">Humidity</div>
+                  </div>
+                </div>
+              </div>
+             </div>
+
+            </div>            
+            
+            
+          <div class="col-md-4" style="font-family: sans-serif;margin-bottom:3.5%;">
+          
+            <div class="card shadow clickable-card" >
+              <div class="card-body text-center">
+                <div class="row">
+                  <div class="col-md-6">
+                    <!--i class="fas fa-umbrella" style="font-size:40px;"></i-->
+                    <img src="../../../../rain.png" alt="Responsive image" class="img-fluid">
+
+                  </div>
+                    <div class="col-md-6">
+                        <h2 class="card-title text-primary">False</h2>
+                        <div class="text-muted">Raining</div>
+                  </div>
+                </div>
+              </div>
+             </div>
+
+            </div>           
+            
+            
+            
+          <div class="col-md-4" style="font-family: sans-serif;margin-bottom:3.5%;">
+          
+            <div class="card shadow clickable-card" >
+              <div class="card-body text-center">
+                <div class="row">
+                  <div class="col-md-6">
+                    <!--i class="fas fa-thermometer-half" style="font-size:40px;"></i-->
+                    <img src="../../../../temperature.png" alt="Responsive image" class="img-fluid">
+
+                  </div>
+                  <div class="col-md-6">
+                    <h2 class="card-title text-primary">12 °C</h2>
+                    <div class="text-muted">Temperature</div>
+                    </div>
+                </div>
+              </div>
+             </div>
+
+            </div>
+        
+        </div> 
+    
+    
+    
+        <div class="row d-flex align-items-stretch" style="font-family: sans-serif;">
+        
+          <div class="col-md-4" style="font-family: sans-serif;">
+          
+            <div class="card shadow clickable-card" >
+              <div class="card-body text-center">
+                <div class="row">
+                  <div class="col-md-6">
+                    <img src="../../../../wind_speed.png" alt="Responsive image" class="img-fluid">
+                  </div>
+                  <div class="col-md-6" style="display: flex; align-items: center; flex-wrap: wrap;">
+                    <h2 class="card-title text-primary">20 Km/h</h2>
+                    <div class="text-muted">Wind Speed</div>
+                  </div>
+                </div>
+              </div>
+             </div>
+
+            </div>   
+            
+            
+          <div class="col-md-4" style="font-family: sans-serif;">
+          
+            <div class="card shadow clickable-card" >
+              <div class="card-body text-center">
+                <div class="row">
+                  <div class="col-md-6">
+                    <!--i class="fas fa-weight" style="font-size:40px;"></i-->
+                    <img src="../../../../pressure.png" alt="Responsive image" class="img-fluid">
+                </div>
+                    <div class="col-md-6" style="display: flex; align-items: center; flex-wrap: wrap;">
+                    <h2 class="card-title text-primary">1020</h2><h5 class="text-primary"> HPA</h5>
+                    <div class="text-muted">Pressure</div>
+                  </div>
+                </div>
+              </div>
+             </div>
+
+            </div>    
+            
+            
+          <div class="col-md-4" style="font-family: sans-serif;">
+          
+            <div class="card shadow clickable-card" >
+              <div class="card-body text-center">
+                <div class="row">
+                  <div class="col-md-6">
+                    <!--i class="fas fa-sun" style="font-size:40px;"></i-->
+                    <img src="../../../../sun.png" alt="Responsive image" class="img-fluid">
+                    
+                    </div>
+                    <div class="col-md-6">
+                    <h2 class="card-title text-primary">60%</h2>
+                    <div class="text-muted">UV Intensity</div>
+                  </div>
+                </div>
+              </div>
+             </div>
+
+            </div>    
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+  
+    }
+
+
+</script>
+
+<style scoped>
+
+
+</style>
